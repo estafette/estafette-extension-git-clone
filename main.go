7c9d102493ab42b3303d8b81c5e151f28a57773d
Jorrit Salverda <jsalverda@travix.com>
2018-12-11 14:19:35 +0100

    use injected credentials for retrieving github or bitbucket api token

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -23,14 +24,14 @@ var (
 	gitOwner             = kingpin.Flag("git-owner", "The owner of the repository.").Envar("ESTAFETTE_GIT_OWNER").Required().String()
 	gitName              = kingpin.Flag("git-name", "The owner plus repository name.").Envar("ESTAFETTE_GIT_NAME").Required().String()
 	gitBranch            = kingpin.Flag("git-branch", "The branch to clone.").Envar("ESTAFETTE_GIT_BRANCH").Required().String()
-	gitRevision          = kingpin.Flag("git-revision", "The revision to check out.").Envar("ESTAFETTE_GIT_REVISION").Required().String()
+	gitRevision          = kingpin.Flag("git-revision", "The revision to check out.").Envar("ESTAFETTE_GIT_REVISION").String()
 	shallowClone         = kingpin.Flag("shallow-clone", "Shallow clone git repository for improved clone time.").Default("true").OverrideDefaultFromEnvar("ESTAFETTE_EXTENSION_SHALLOW").Bool()
 	overrideRepo         = kingpin.Flag("override-repo", "Set other repository name to clone from same owner.").Envar("ESTAFETTE_EXTENSION_REPO").String()
 	overrideBranch       = kingpin.Flag("override-branch", "Set other repository branch to clone from same owner.").Envar("ESTAFETTE_EXTENSION_BRANCH").String()
 	overrideSubdirectory = kingpin.Flag("override-directory", "Set other repository directory to clone from same owner.").Envar("ESTAFETTE_EXTENSION_SUBDIR").String()
 
-	bitbucketAPIToken = kingpin.Flag("bitbucket-api-token", "The api token in case it's a bitbucket repo.").Envar("ESTAFETTE_BITBUCKET_API_TOKEN").String()
-	githubAPIToken    = kingpin.Flag("github-api-token", "The api token in case it's a github repo.").Envar("ESTAFETTE_GITHUB_API_TOKEN").String()
+	bitbucketAPITokenJSON = kingpin.Flag("bitbucket-api-token", "Bitbucket api token credentials configured at the CI server, passed in to this trusted extension.").Envar("ESTAFETTE_CREDENTIALS_BITBUCKET_API_TOKEN").String()
+	githubAPITokenJSON    = kingpin.Flag("github-api-token", "Github api token credentials configured at the CI server, passed in to this trusted extension.").Envar("ESTAFETTE_CREDENTIALS_GITHUB_API_TOKEN").String()
 )
 
 func main() {
@@ -45,6 +46,33 @@ func main() {
 	// log startup message
 	log.Printf("Starting estafette-extension-git-clone version %v...", version)
 
+	// get api token from injected credentials
+	bitbucketAPIToken := ""
+	if *bitbucketAPITokenJSON != "" {
+		var credentials []APITokenCredentials
+		err := json.Unmarshal([]byte(*bitbucketAPITokenJSON), &credentials)
+		if err != nil {
+			log.Fatal("Failed unmarshalling injected credentials: ", err)
+		}
+		if len(credentials) == 0 {
+			log.Fatal("No credentials have been injected")
+		}
+		bitbucketAPIToken = credentials[0].AdditionalProperties.Token
+	}
+
+	githubAPIToken := ""
+	if *githubAPITokenJSON != "" {
+		var credentials []APITokenCredentials
+		err := json.Unmarshal([]byte(*githubAPITokenJSON), &credentials)
+		if err != nil {
+			log.Fatal("Failed unmarshalling injected credentials: ", err)
+		}
+		if len(credentials) == 0 {
+			log.Fatal("No credentials have been injected")
+		}
+		githubAPIToken = credentials[0].AdditionalProperties.Token
+	}
+
 	if *overrideRepo != "" {
 		if *overrideBranch == "" {
 			*overrideBranch = "master"
@@ -54,11 +82,11 @@ func main() {
 		}
 
 		overrideGitURL := ""
-		if *bitbucketAPIToken != "" {
-			overrideGitURL = fmt.Sprintf("https://x-token-auth:%v@%v/%v/%v", *bitbucketAPIToken, *gitSource, *gitOwner, *overrideRepo)
+		if bitbucketAPIToken != "" {
+			overrideGitURL = fmt.Sprintf("https://x-token-auth:%v@%v/%v/%v", bitbucketAPIToken, *gitSource, *gitOwner, *overrideRepo)
 		}
-		if *githubAPIToken != "" {
-			overrideGitURL = fmt.Sprintf("https://x-access-token:%v@%v/%v/%v", *githubAPIToken, *gitSource, *gitOwner, *overrideRepo)
+		if githubAPIToken != "" {
+			overrideGitURL = fmt.Sprintf("https://x-access-token:%v@%v/%v/%v", githubAPIToken, *gitSource, *gitOwner, *overrideRepo)
 		}
 
 		if overrideGitURL == "" {
@@ -75,11 +103,11 @@ func main() {
 	}
 
 	gitURL := ""
-	if *bitbucketAPIToken != "" {
-		gitURL = fmt.Sprintf("https://x-token-auth:%v@%v/%v/%v", *bitbucketAPIToken, *gitSource, *gitOwner, *gitName)
+	if bitbucketAPIToken != "" {
+		gitURL = fmt.Sprintf("https://x-token-auth:%v@%v/%v/%v", bitbucketAPIToken, *gitSource, *gitOwner, *gitName)
 	}
-	if *githubAPIToken != "" {
-		gitURL = fmt.Sprintf("https://x-access-token:%v@%v/%v/%v", *githubAPIToken, *gitSource, *gitOwner, *gitName)
+	if githubAPIToken != "" {
+		gitURL = fmt.Sprintf("https://x-access-token:%v@%v/%v/%v", githubAPIToken, *gitSource, *gitOwner, *gitName)
 	}
 
 	if gitURL == "" {
